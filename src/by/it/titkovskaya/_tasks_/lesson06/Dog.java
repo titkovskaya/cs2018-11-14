a68b4cf7d79dbf9e1f3c2f49d1bbb51a750882b3
Airina <i.titkovskaya@gmail.com>
2018-11-26 12:53:25 +0300

    Lesson01-06 completed

--- src/by/it/titkovskaya/_tasks_/lesson06/Dog.java
+++ src/by/it/titkovskaya/_tasks_/lesson06/Dog.java
@@ -3,6 +3,8 @@ package by.it.titkovskaya._tasks_.lesson06;
 public class Dog {
     private String name;
     private int age;
+    private int weight; //вес собаки
+    private double power; //сила укуса собаки
 
     public String getName() {
         return name;
@@ -20,9 +22,20 @@ public class Dog {
         this.age = age;
     }
 
-    @Override
-    public String toString() {
-        return "Кличка: "+name+". Возраст: "+age;
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
     }
 
     public Dog(String name, int age) {
@@ -31,5 +44,23 @@ public class Dog {
     }
 
     public Dog() {
+    }
+
+    public Dog(String name, int age, int weight, double power) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.power = power;
+    }
+
+    boolean win(Dog otherDog) {
+        double myChance=0.2 * this.age + 0.3 * this.weight + 0.5 * this.power;
+        double herChance=0.2 * otherDog.age + 0.3 * otherDog.weight + 0.5 * otherDog.power;
+        return myChance>herChance;
+    }
+
+    @Override
+    public String toString() {
+        return "Кличка: "+name+". Возраст: "+age;
     }
 }
